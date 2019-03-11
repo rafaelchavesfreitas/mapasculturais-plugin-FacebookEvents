a545801808d0faea92fd41064b35cc748af90746
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2019-03-11 14:28:19 -0300

    cria esqueleto do plugin

---
+++ plugins/FacebookEvents/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+namespace FacebookEvents;
+
+class Plugin extends \MapasCulturais\Plugin {
+    public function _init() {
+        // enqueue scripts and styles
+
+        // add hooks
+    }
+
+    public function register() {
+        // register metadata, taxonomies
+
+    }
+}
